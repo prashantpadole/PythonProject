559c2eb5f8fb520a0c2e5117c071e023d683cad2
prashantprashant <prashant.padole@outlook.com>
2021-12-03 00:41:54 -0500

    t

--- practice/GeneratorExample.py
+++ practice/GeneratorExample.py
@@ -1,1 +1,47 @@
 
+import memory_profiler as mem_profile
+import random
+import time
+
+names=["Johnson","David","Rox","Newton","Albeta","Emily","Joy"]
+majors=["Math","English","French","Economics","Computer sci.","Business"]
+
+print("memory before {} MB".format(mem_profile.memory_usage()))
+def people_list(num_people):
+    result=[]
+    for i in range(num_people):
+        person={
+            'id':i,
+            'name':random.choice(names),
+            'major':random.choice(majors)
+        }
+        result.append(person)
+    return result
+
+
+def people_generator(num_people):
+    for i in range(num_people):
+        person={
+            'id':i,
+            'name':random.choice(names),
+            'major':random.choice(majors)
+        }
+
+        yield person
+
+t1=time.time()
+# people=people_list(10000000)
+people= people_generator(10000000)
+t2=time.time()
+
+
+
+
+for i in people:
+    print(i)
+
+print("Memory after {} MB".format(mem_profile.memory_usage()))
+print("Totel execution time is {}".format(t2-t1))
+
+
+
